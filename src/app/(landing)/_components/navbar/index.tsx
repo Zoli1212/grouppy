66c9140b0458afd96c0da6e431c4735a33acd0bd
butterfly-react <rdigital4455@gmail.com>
2024-09-25 23:04:36 +0200

    commit üzenet

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react"
 
 type Props = {
-
     children: React.ReactNode
 }
 
 function LandingPageNavbar({ children }: Props) {
-  return (
-    <div>LandingPageNavbar</div>
-  )
+    return <div>LandingPageNavbar</div>
 }
 
 export default LandingPageNavbar
--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import LandingPageNavbar from './_components/navbar'
+import React from "react"
+import LandingPageNavbar from "./_components/navbar"
 
 type Props = {
-
     children: React.ReactNode
 }
 
 function LandingPageLayout({ children }: Props) {
-  return (
-    <div className='flex felx-col container relative'>
-
-        <LandingPageNavbar>{children}</LandingPageNavbar>
-    </div>
-  )
+    return (
+        <div className="flex felx-col container relative">
+            <LandingPageNavbar>{children}</LandingPageNavbar>
+        </div>
+    )
 }
 
 export default LandingPageLayout
