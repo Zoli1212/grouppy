644283e5dc61ff3215179f56e6810478567ff832
butterfly-react <rdigital4455@gmail.com>
2024-10-03 20:51:49 +0200

    your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "ucarecdn.com",
-        },
-      ],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "ucarecdn.com",
+            },
+        ],
     },
-  }
-  
-  export default nextConfig
+}
+
+export default nextConfig
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,22 +3,22 @@ import dynamic from "next/dynamic"
 import DashboardSnippet from "./_components/dashboard-snippet"
 import CallToAction from "./_components/call-to-action"
 
-// const PricingSection = dynamic(
-//   () =>
-//     import("./_components/pricing").then(
-//       (component) => component.PricingSection,
-//     ),
-//   { ssr: true },
-// )
+const PricingSection = dynamic(
+  () =>
+    import("./_components/pricing").then(
+      (component) => component.PricingSection,
+    ),
+  { ssr: true },
+)
 
 export default function Home() {
-  return (
-    <main className="md:px-10 py-20 flex flex-col gap-36">
-      <div>
-        <CallToAction />
-        <DashboardSnippet />
-      </div>
-      {/* <PricingSection /> */}
-    </main>
-  )
+    return (
+        <main className="md:px-10 py-20 flex flex-col gap-36">
+            <div>
+                <CallToAction />
+                <DashboardSnippet />
+            </div>
+            <PricingSection />
+        </main>
+    )
 }
