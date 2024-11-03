de46c5b69a65fada44fe379e6b1f8863685356a6
butterfly-react <rdigital4455@gmail.com>
2024-11-03 23:49:08 +0100

    9:42

--- src/app/group/[groupid]/settings/subscriptions/page.tsx
+++ src/app/group/[groupid]/settings/subscriptions/page.tsx
@@ -2,16 +2,20 @@ import { GroupSubscriptionForm } from "@/components/forms/subscription"
 import { Subscriptions } from "./_components/subscriptions"
 
 type Props = {
-  params: { groupid: string }
+  params: Promise<{ groupid: string }>
 }
 
-const SubscriptionPage = ({ params }: Props) => {
+
+
+  const SubscriptionPage = async ({ params }: Props) => {
+    const { groupid } = await params
+
   return (
     <div className="p-10 flex flex-col gap-y-10">
       <h2 className="font-bold text-3xl">Group Subscriptions</h2>
       <div className="grid grid-cols-1 lg:grid-cols-5 gap-5">
-        <GroupSubscriptionForm groupid={params.groupid} />
-        <Subscriptions groupid={params.groupid} />
+        <GroupSubscriptionForm groupid={groupid} />
+        <Subscriptions groupid={groupid} />
       </div>
     </div>
   )
