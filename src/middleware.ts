9a478147bcd32a23387e6b7defc719db6079a36e
butterfly-react <rdigital4455@gmail.com>
2024-11-04 15:16:30 +0100

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,20 +8,33 @@ export default clerkMiddleware(async (auth, req) => {
   const host = req.headers.get("host")
   const reqPath = req.nextUrl.pathname
   const origin = req.nextUrl.origin
-  if (isProtectedRoute(req)) await auth.protect()
-  if (!baseHost.includes(host as string) && reqPath.includes("/group")) {
-    const response = await fetch(`${origin}/api/domain?host=${host}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
 
-    const data = await response.json()
-    if (data.status === 200 && data) {
-      return NextResponse.rewrite(
-        new URL(reqPath, `https://${data.domain}/${reqPath}`),
-      )
+  // Protect route if it's a protected route
+  if (isProtectedRoute(req)) await auth.protect()
+
+  // Proceed with the domain check if not on base host
+  if (!baseHost.includes(host as string) && reqPath.includes("/group")) {
+    try {
+      const response = await fetch(`${origin}/api/domain?host=${host}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+
+      // Only parse JSON if the response status is 200
+      if (response.ok) {
+        const data = await response.json()
+        if (data && data.status === 200) {
+          return NextResponse.rewrite(
+            new URL(reqPath, `https://${data.domain}/${reqPath}`)
+          )
+        }
+      } else {
+        console.error(`Unexpected response status: ${response.status}`)
+      }
+    } catch (error) {
+      console.error("Error fetching domain data:", error)
     }
   }
 })
